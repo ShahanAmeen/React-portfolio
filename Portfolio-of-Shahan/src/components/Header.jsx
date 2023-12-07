e01849b06aa85e966b43c4c9ec82167b449f3569
Shahan Ameen <Shahanameen@gmail.com>
2023-12-07 15:01:02 -0600

    updated

--- Portfolio-of-Shahan/src/components/Header.jsx
+++ Portfolio-of-Shahan/src/components/Header.jsx
@@ -1,11 +1,29 @@
-
+import {Link} from "react-router-dom"
+import "./Header.css"
+import headshot from "./ShahanUpdatedlinkedIn.jpg"
 function Header(){
   return(
-    <div>
-<h2>
-  This is the Header
-</h2>
+    
 
+  <div className="div-flex">
+    <div className="header-info">
+<img id="image" src={headshot} alt="Shahan professionalpic" />
+<h4 className="main-name">
+  Shahan Ameen
+</h4>
+<h4 className="main-name">
+  Junior Developer 
+</h4>
+   </div>
+
+<ul>
+  
+  <Link to="/"><li>About</li></Link>
+  <Link to="/portfolio"><li>Portfolio</li></Link>
+  <Link to="/contact"><li>Contact</li></Link>
+  <Link to="/resume"><li>Resume</li></Link>
+
+</ul>
 
     </div>
   )
--- Portfolio-of-Shahan/src/pages/PortfolioPage.jsx
+++ Portfolio-of-Shahan/src/pages/PortfolioPage.jsx
@@ -1,3 +1,6 @@
+import image1 from "./cravingswinebarimg.jpg"
+import image2 from "./cravingswinebarimg.jpg"
+
 function Portfolio() {
   return (
     <div>
@@ -5,7 +8,9 @@ function Portfolio() {
         This is the Portfolio
       </h2>
 
-
+      <a href="https://nestibry.github.io/pet-space/   " ><img id="image2" src={image1} alt="Cravings"/></a>
+      <a href="https://shahanameen.github.io/prework-study-guide2/"> <img id="prework" src={image2} alt="Preworkstudyguide" /></a>
+      <a href=" https://shahanameen.github.io/hw-01/"> <img id="seo" src="assets/seo.png" alt="website correction"/></a>
     </div>
   )
 }
