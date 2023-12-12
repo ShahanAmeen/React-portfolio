67c31035198a995c5ba3600317e00e0526242d43
Shahan Ameen <Shahanameen@gmail.com>
2023-12-12 12:58:18 -0600

    readme included

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,4 +1,4 @@
-import image1 from "./cravingswinebarimg.jpg"
+
 import image2 from "./petSpace.png"
 import image3 from "./weatherForecast.png"
 import image4 from "./flashcardDatabase.png"
@@ -11,12 +11,11 @@ function Portfolio() {
         This is the Portfolio
       </h2>
 
-      <a href="https://nestibry.github.io/pet-space/   " ><img id="image2" src={image1} alt="Cravings"/></a>
-      <a href="https://shahanameen.github.io/prework-study-guide2/"> <img id="prework" src={image2} alt="Preworkstudyguide" /></a>
-      <a href=" https://shahanameen.github.io/hw-01/"> <img id="seo" src={image3} alt="website correction"/></a>
-      <a href="https://nestibry.github.io/pet-space/   " ><img id="image2" src={image4} alt="Cravings" /></a>
-      <a href="https://shahanameen.github.io/prework-study-guide2/"> <img id="prework" src={image2} alt="Preworkstudyguide" /></a>
-      <a href=" https://shahanameen.github.io/hw-01/"> <img id="seo" src={image2} alt="website correction" /></a>
+      
+      <a href="https://nestibry.github.io/pet-space/"> <img id="prework" src={image2} alt="pet-space" /></a>
+      <a href=" https://shahanameen.github.io/Weather-API/"> <img id="seo" src={image3} alt="Weather-API"/></a>
+      <a href="https://flash-card-database-b48354848dbb.herokuapp.com/ " ><img id="image2" src={image4} alt="Flash-Card" /></a>
+   
     
     </div>
   )
